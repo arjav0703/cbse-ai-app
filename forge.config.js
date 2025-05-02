b88236fb2e7b34df9d587d787ec072c43c25528c
Arjav Jain <arjavjain0703@gmail.com>
2025-05-02 19:56:39 +0530

    integrate multi build

--- forge.config.js
+++ forge.config.js
@@ -32,12 +32,12 @@ module.exports = {
       name: "@electron-forge/maker-deb",
       config: {},
     },
-    // {
-    //   name: "@electron-forge/maker-dmg",
-    //   config: {
-    //     format: "ULFO",
-    //   },
-    // },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        format: "ULFO",
+      },
+    },
   ],
   plugins: [
     {
