887afcd3a73ebd07f402aa978a13781422a4f905
Arjav Jain <arjavjain0703@gmail.com>
2025-05-02 16:34:51 +0530

    update app

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,11 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#google-signin-btn");
+  if (button) {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      require("electron").shell.openExternal("/api/auth/auth/google");
+    });
+  }
+});
